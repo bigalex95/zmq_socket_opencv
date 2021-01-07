df3729d7071850e25a5d609f7832d1830092e9e7
Alibek <alibek060395@gmail.com>
2021-01-07 14:32:29 +0300

    edit frame names

--- TCPsocket/imagizServer2.py
+++ TCPsocket/imagizServer2.py
@@ -14,7 +14,7 @@ def main():
         try:
             message=server.receive()
             frame = cv2.imdecode(message.image,1)
-            cv2.imshow("frame1",frame)
+            cv2.imshow("frame2",frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             fps.update()
